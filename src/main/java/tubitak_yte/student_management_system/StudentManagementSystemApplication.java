03eb9d865e5564a48f153cdccabeef29d7739e11
pelinsugok <pelinsugok@gmail.com>
2023-03-08 01:46:37 +0300

    Spring security settings added

--- src/main/java/tubitak_yte/student_management_system/StudentManagementSystemApplication.java
+++ src/main/java/tubitak_yte/student_management_system/StudentManagementSystemApplication.java
@@ -1,13 +1,16 @@
 package tubitak_yte.student_management_system;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+
 public class StudentManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
 
 }
